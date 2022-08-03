a2f32b6736c3a4f24b8f70547ecab6ddf1876535
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-08-03 00:16:48 -0300

    feat: add setupFilesAfterEnv on jest config

--- jest.config.ts
+++ jest.config.ts
@@ -135,7 +135,9 @@ export default {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/test/setup.ts'
+  ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
