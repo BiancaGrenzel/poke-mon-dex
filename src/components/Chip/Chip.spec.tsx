f3ac543cf38d58e959756d31e7d2e611b3011e55
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-08-03 00:16:00 -0300

    feat: Chip unit tests

---
+++ src/components/Chip/Chip.spec.tsx
@@ -0,0 +1,30 @@
+import { render, fireEvent } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import * as Theme from "../../styles/tokens/index";
+import "jest-styled-components";
+import Chip from "./index";
+
+describe("Chip Component", () => {
+  it("should render children", () => {
+    const { getByTestId } = render(
+      <ThemeProvider theme={Theme}>
+        <Chip pokemonType="dragon" children="Teste" />
+      </ThemeProvider>
+    );
+
+    expect(getByTestId("chip-text")).toBeInTheDocument();
+  });
+
+  it("should render with correct color", () => {
+    const { getByTestId } = render(
+      <ThemeProvider theme={Theme}>
+        <Chip pokemonType="dragon" children="Teste" />
+      </ThemeProvider>
+    );
+
+    expect(getByTestId("chip-component")).toHaveStyleRule(
+      "background-color",
+      Theme.colors.dragon
+    );
+  });
+});
