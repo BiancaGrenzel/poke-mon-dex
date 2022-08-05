89250d2ec1a46a5e29253c4f9643f98d70949a85
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-08-04 23:55:08 -0300

    feat: not found image on photo

--- src/components/Photo/index.tsx
+++ src/components/Photo/index.tsx
@@ -1,10 +1,16 @@
-import { Container, StyledImage } from "./style";
+import { Container, StyledImage, StyledObject } from "./style";
 import { PhotoProps } from "./types";
 
 const Photo = ({ src, alt, id }: PhotoProps) => {
   return (
     <Container data-testid={`photo-container-${id}`} id={id}>
-      <StyledImage src={src} alt={alt}/>
+      <StyledObject data={src} data-testid={`object-${id}`}>
+        <StyledImage
+          src="https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"
+          alt={alt}
+          data-testid="error-photo"
+        />
+      </StyledObject>
     </Container>
   );
 };
