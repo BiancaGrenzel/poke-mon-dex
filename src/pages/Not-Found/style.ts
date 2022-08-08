5a5a07916792f8cc270e9b3b42530e3e4b017caf
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-08-08 18:56:39 -0300

    feat: NotFound Page

---
+++ src/pages/Not-Found/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 128px);
+  width: calc(100vw - 480px);
+  gap: 32px;
+  background-color: #f9f9f9;
+  padding: 0px 240px;
+  margin: -64px 0px 0px 0px;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
