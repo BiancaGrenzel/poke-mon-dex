2fc9bef024d5411c8effd473a282ac4827259b9d
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-08-08 17:22:45 -0300

    feat: use filterPokemons util

--- src/components/Card-List/index.tsx
+++ src/components/Card-List/index.tsx
@@ -2,54 +2,49 @@ import { useEffect, useState } from "react";
 import { getPokemons } from "../../services/api";
 import { useDataContext } from "../../store/context";
 import { Pokemon } from "../../types";
+import { filterPokemons } from "../../utils/filterPokemons";
 import Card from "../Card/index";
 import { ContainerCards } from "./style";
 
 const CardList = () => {
-  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
-  const [page, setPage] = useState(1);
-  const [totalPage, setTotalPage] = useState(0);
-  const { oldOffsetHeight } = useDataContext();
-  let postsForPage = 25;
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [page]);
+  const { filteredTypes, allPokemons, setAllPokemons, search } =
+    useDataContext();
+  const [filteredPokemons, setFilteredPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
     getPokemons().then((data) => {
-      setTotalPage(data.length / postsForPage);
-      let newPokemons = data.splice(allPokemons.length, postsForPage);
-      setAllPokemons([...allPokemons, ...newPokemons]);
+      setAllPokemons(data);
     });
-  }, [page]);
+  }, []);
 
-  function handleScroll() {
-    if (
-      window.innerHeight + window.scrollY >=
-      document.body.offsetHeight - oldOffsetHeight
-    ) {
-      setPage(page + 1);
-    }
-    return;
-  }
+  useEffect(() => {
+    const filtered = filterPokemons({
+      allPokemons: allPokemons,
+      text: search,
+      type: filteredTypes,
+    });
+    setFilteredPokemons([...(filtered || allPokemons)]);
+  }, [allPokemons, filteredTypes, search]);
+
+  const renderPokemons = () => {
+    return filteredPokemons.map((pokemon, index) => {
+      return (
+        <Card
+          data-testid={`card-${index}`}
+          key={index}
+          pokemonNumber={pokemon.national_number}
+          pokemonName={pokemon.name}
+          srcPhoto={pokemon.sprites.animated}
+          altPhoto={pokemon.name}
+          pokemonType={pokemon.type}
+        />
+      );
+    });
+  };
 
   return (
     <ContainerCards data-testid="container-cards">
-      {allPokemons.map((pokemon, index) => {
-        return (
-          <Card
-            data-testid={`card-${index}`}
-            key={index}
-            pokemonNumber={pokemon.national_number}
-            pokemonName={pokemon.name}
-            srcPhoto={pokemon.sprites.animated}
-            altPhoto={pokemon.name}
-            pokemonType={pokemon.type}
-          />
-        );
-      })}
+      {renderPokemons()}
     </ContainerCards>
   );
 };
