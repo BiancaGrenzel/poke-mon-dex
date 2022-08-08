c2064ecc6a1872f1f1984a2a55606933c3f50a02
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-08-08 17:27:02 -0300

    feat: add Input component on Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,27 @@
 import CardList from "../../components/Card-List";
+import FilterCard from "../../components/Filter-Card";
+import Input from "../../components/Input";
+import { Box, Section } from "../../layouts";
+import { useDataContext } from "../../store/context";
 
 const Home = () => {
+  const {
+    search,
+    setSearch,
+  } = useDataContext();
+
   return (
-    <div>
-      <CardList />
-    </div>
+    <Section>
+      <Input
+        placeholder="Pesquisar por nome ou número"
+        value={search}
+        onChange={(e: any) => setSearch(e.target.value)}
+      />
+      <Box justifyContent="space-around" width="100%">
+        <FilterCard />
+        <CardList />
+      </Box>
+    </Section>
   );
 };
 
