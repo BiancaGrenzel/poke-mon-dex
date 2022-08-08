2c926585b6407c207373fe3594acccd1aba09a62
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-08-08 17:22:56 -0300

    fix: style on ContainerCards

--- src/components/Card-List/style.ts
+++ src/components/Card-List/style.ts
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
 export const ContainerCards = styled.div`
-    display: flex;
-    gap: ${({ theme }) => theme.sizes.lg};
-    flex-wrap: wrap;
-`
+  display: flex;
+  gap: ${({ theme }) => theme.sizes.lg};
+  flex-wrap: wrap;
+  max-height: calc(100vh - 240px);
+  overflow: auto;
+  justify-self: flex-end;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+`;
